e3ed7985765aca1c204ce01e1744f98253f19f2d
ChaseMilligan <chasemilligan22@gmail.com>
2021-08-24 14:57:55 -0600

    maybe deploy now?

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Grommet } from "grommet";
+import * as serviceWorker from "./serviceWorker";
 
-const theme = {
-  global: {
-    font: {
-      family: "Roboto",
-      size: "14px",
-      height: "20px",
-    },
-  },
-};
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <Grommet theme={theme}>
-    <App />
-  </Grommet>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
