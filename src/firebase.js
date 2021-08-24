11e7e71b30eb5d7c20492fcca046ae679158cdff
ChaseMilligan <chasemilligan22@gmail.com>
2021-08-24 15:02:43 -0600

    maybe deploy now?

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDJyY9NZcwL7f6zxWHUhS9K4ZLhSNWpZec",
+  authDomain: "house-rules-4ffbc.firebaseapp.com",
+  databaseURL: "https://house-rules-4ffbc-default-rtdb.firebaseio.com",
+  projectId: "house-rules-4ffbc",
+  storageBucket: "house-rules-4ffbc.appspot.com",
+  messagingSenderId: "573758979687",
+  appId: "1:573758979687:web:c39ab2fd91ee39b0c41ec2",
+  measurementId: "G-V8XMT1XFTD",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage().ref();
