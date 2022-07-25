424a246c97d9d9a3d140c075a8c0a77d1026d277
ChaseMilligan <chasemilligan22@gmail.com>
2022-07-25 17:00:23 -0600

    manifest

---
+++ public/service-worker.js
@@ -0,0 +1,51 @@
+let CACHE_NAME = 'houseRules1';
+let urlsToCache = [
+    'index.html'
+];
+
+const self = this;
+
+self.addEventListener('install', function (event)
+{
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache)
+            {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event)
+{
+    event.respondWith(
+        caches.match(event.request)
+            .then(function ()
+            {
+                console.log('Fetched');
+                return fetch(event.request).catch(() =>
+                {
+                    caches.match('index.html');
+                })
+            })
+    );
+});
+
+self.addEventListener('activate', function (event)
+{
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) =>
+            {
+                if (!cacheWhitelist.includes(cacheName))
+                {
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    );
+});
