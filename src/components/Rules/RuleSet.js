c6dde94e028dd4bbd8c56a7f6d6ee0181ba74921
Chase Milligan <chase@stealthmedia.com>
2022-08-17 16:49:22 -0600

    starting dnd for rules

--- src/components/Rules/RuleSet.js
+++ src/components/Rules/RuleSet.js
@@ -3,99 +3,50 @@ import update from 'immutability-helper';
 import { FormNextLink, Drag } from 'grommet-icons';
 import { useState, useCallback, useRef } from 'react';
 import RuleSetModal from './RuleSetModal';
-import { useDrag, useDrop } from 'react-dnd';
+import { Draggable } from 'react-beautiful-dnd';
 
 export default function RuleSet(props) {
-	const ref = useRef(null);
-	const [{ handlerId }, drop] = useDrop({
-		accept: 'box',
-		collect(monitor) {
-			return {
-				handlerId: monitor.getHandlerId()
-			};
-		},
-		hover(item, monitor) {
-			if (!ref.current) {
-				return;
-			}
-			const dragIndex = item.index;
-			const hoverIndex = props.index;
-			// Don't replace items with themselves
-			if (dragIndex === hoverIndex) {
-				return;
-			}
-			// Determine rectangle on screen
-			const hoverBoundingRect = ref.current?.getBoundingClientRect();
-			// Get vertical middle
-			const hoverMiddleY =
-				(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-			// Determine mouse position
-			const clientOffset = monitor.getClientOffset();
-			// Get pixels to the top
-			const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-			// Only perform the move when the mouse has crossed half of the items height
-			// When dragging downwards, only move when the cursor is below 50%
-			// When dragging upwards, only move when the cursor is above 50%
-			// Dragging downwards
-			if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-				return;
-			}
-			// Dragging upwards
-			if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-				return;
-			}
-			// Time to actually perform the action
-			console.log(dragIndex, hoverIndex);
-			props.moveCard(dragIndex, hoverIndex);
-			// Note: we're mutating the monitor item here!
-			// Generally it's better to avoid mutations,
-			// but it's good here for the sake of performance
-			// to avoid expensive index searches.
-			item.index = hoverIndex;
-		}
-	});
-	const [{ isDragging }, drag] = useDrag({
-		type: 'box',
-		item: () => {
-			return { id: props.id, index: props.index };
-		},
-		collect: (monitor) => ({
-			isDragging: monitor.isDragging()
-		})
-	});
-	const opacity = isDragging ? 0.7 : 1;
-
-	drag(drop(ref));
-
 	return (
-		<Box
-			draggable
-			className={isDragging ? 'rule-item is-dragging' : 'rule-item'}
-			margin={{ bottom: '1em' }}
-			background="light-2"
+		<Draggable
 			key={props.index}
-			ref={ref}
-			style={{ opacity: opacity }}
-			data-handler-id={handlerId}
+			draggableId={'drag-' + props.index}
+			index={props.index}
 		>
-			<Box flex direction="row" align="center" justify="between">
-				<Box flex direction="row" align="center" justify="start">
-					{props.canEdit && <Drag ref={drag} />}
-					<Heading level="3" margin={{ left: '1em' }}>
-						{props.ruleSet.name}
-					</Heading>
+			{(provided, snapshot) => (
+				<Box
+					className={
+						snapshot.isDragging ? 'rule-item is-dragging' : 'rule-item'
+					}
+					style={{
+						...provided.draggableProps.style
+					}}
+					margin={{ bottom: '1em' }}
+					background="light-2"
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+				>
+					<Box flex direction="row" align="center" justify="between">
+						<Box flex direction="row" align="center" justify="start">
+							<div {...provided.dragHandleProps}>
+								<Drag />
+							</div>
+							<Heading level="3" margin={{ left: '1em' }}>
+								{props.ruleSet.name}
+							</Heading>
+						</Box>
+						<div>
+							<Button
+								size="small"
+								icon={<FormNextLink color="dark-6" />}
+								onClick={() => {
+									props.setRuleSet(props.ruleSet);
+									props.setViewRuleSet(true);
+								}}
+							/>
+						</div>
+					</Box>
 				</Box>
-				<div>
-					<Button
-						size="small"
-						icon={<FormNextLink color="dark-6" />}
-						onClick={() => {
-							props.setRuleSet(props.ruleSet);
-							props.setViewRuleSet(true);
-						}}
-					/>
-				</div>
-			</Box>
-		</Box>
+			)}
+		</Draggable>
 	);
 }
--- src/components/Rules/RuleSetList.js
+++ src/components/Rules/RuleSetList.js
@@ -3,39 +3,13 @@ import update from 'immutability-helper';
 import { FormNextLink, Drag } from 'grommet-icons';
 import { useState, useCallback, useRef } from 'react';
 import RuleSetModal from './RuleSetModal';
-import { useDrag, useDrop } from 'react-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import RuleSet from './RuleSet';
 
 export default function RuleSetList(props) {
 	const [ruleSets, setRuleSets] = useState(props.ruleSets);
 	const [viewRuleSet, setViewRuleSet] = useState(false);
 	const [ruleSet, setRuleSet] = useState();
-
-	const moveCard = useCallback((dragIndex, hoverIndex) => {
-		setRuleSets((prevCards) =>
-			update(prevCards, {
-				$splice: [
-					[dragIndex, 1],
-					[hoverIndex, 0, prevCards[dragIndex]]
-				]
-			})
-		);
-	}, []);
-
-	const renderCard = useCallback((ruleSet, index) => {
-		return (
-			<RuleSet
-				key={ruleSet.id}
-				index={index}
-				id={ruleSet.id}
-				text={ruleSet.text}
-				ruleSet={ruleSet}
-				moveCard={moveCard}
-				setRuleSet={setRuleSet}
-				canEdit={props.canEdit}
-			/>
-		);
-	}, []);
 
 	return (
 		<Box margin="1em 0px" flex fill>
@@ -47,9 +21,33 @@ export default function RuleSetList(props) {
 					handleDeleteRuleSet={props.handleDeleteRuleSet}
 				/>
 			)}
-			<div className="drag-container">
-				{ruleSets.map((ruleSet, index) => renderCard(ruleSet, index))}
-			</div>
+			<DragDropContext
+				onDragEnd={(...props) => {
+					console.log(props);
+				}}
+			>
+				<Droppable droppableId="drop-1">
+					{(provided) => (
+						<div
+							className="drag-container"
+							ref={provided.innerRef}
+							{...provided.droppableProps}
+						>
+							{ruleSets.map((ruleSet, index) => (
+								<RuleSet
+									key={ruleSet.id}
+									index={index}
+									id={ruleSet.id}
+									text={ruleSet.text}
+									ruleSet={ruleSet}
+									setRuleSet={setRuleSet}
+									canEdit={props.canEdit}
+								/>
+							))}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
 		</Box>
 	);
 }
